db9901aced459b4e493f741b1c1e71c01d06453f
Evils <evils.devils@protonmail.com>
2020-04-11 23:22:23 +0200

    error-inject: init mce, edac and aer

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19131,6 +19131,8 @@ in
 
   ebtables = callPackage ../os-specific/linux/ebtables { };
 
+  error-inject = callPackages ../os-specific/linux/error-inject { };
+
   extrace = callPackage ../os-specific/linux/extrace { };
 
   facetimehd-firmware = callPackage ../os-specific/linux/firmware/facetimehd-firmware { };
