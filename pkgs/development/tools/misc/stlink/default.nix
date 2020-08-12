d2493313020c2307bd208f5d7d82c3c2dd46233b
Evils <evils.devils@protonmail.com>
2020-08-12 21:16:20 +0200

    stlink: 1.6.0 -> 2020-08-09
    1.6.1 was released, this rev is 88 commits further
    because somewhere in those 88 is a fix that makes it build

--- pkgs/development/tools/misc/stlink/default.nix
+++ pkgs/development/tools/misc/stlink/default.nix
@@ -1,31 +1,30 @@
-{ stdenv, fetchFromGitHub, cmake, libusb1 }:
+{ stdenv, fetchFromGitHub, cmake, libusb1, fetchpatch }:
 
 let
   # The Darwin build of stlink explicitly refers to static libusb.
   libusb1' = if stdenv.isDarwin then libusb1.override { withStatic = true; } else libusb1;
 
 # IMPORTANT: You need permissions to access the stlink usb devices.
-# Add services.udev.pkgs = [ pkgs.stlink ] to your configuration.nix
+# Add services.udev.packages = [ pkgs.stlink ] to your configuration.nix
 
 in stdenv.mkDerivation rec {
   pname = "stlink";
-  version = "1.6.0";
+  version = "2020-08-09";
 
   src = fetchFromGitHub {
-    owner = "texane";
+    owner = "stlink-org";
     repo = "stlink";
-    rev = "v${version}";
-    sha256 = "1mlkrxjxg538335g59hjb0zc739dx4mhbspb26z5gz3lf7d4xv6x";
+    rev = "0a6fe3a8530c950def7b223527574ecf6798d956";
+    sha256 = "0sncrqzp97abqgscw69vmjzbbalpisnvcra24rhnsmwdijdmlav3";
   };
 
   buildInputs = [ libusb1' ];
   nativeBuildInputs = [ cmake ];
   patchPhase = ''
-    sed -i 's@/etc/udev/rules.d@$ENV{out}/etc/udev/rules.d@' CMakeLists.txt
+    sed -i 's@/lib/udev/rules.d@$ENV{out}/lib/udev/rules.d@' CMakeLists.txt
     sed -i 's@/etc/modprobe.d@$ENV{out}/etc/modprobe.d@' CMakeLists.txt
   '';
   preInstall = ''
-    mkdir -p $out/etc/udev/rules.d
     mkdir -p $out/etc/modprobe.d
   '';
 
