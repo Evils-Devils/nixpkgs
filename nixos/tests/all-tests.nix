bfdbe04f31702e834fd4fd6cf6440cecacf3e4b4
Evils <evils.devils@protonmail.com>
2021-02-01 20:05:21 +0100

    nixosTests.rasdaemon-edac: no ECC in VM?

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -331,6 +331,7 @@ in
   radarr = handleTest ./radarr.nix {};
   radicale = handleTest ./radicale.nix {};
   rasdaemon = handleTest ./rasdaemon.nix {};
+  rasdaemon-edac = handleTest ./rasdaemon-edac.nix {};
   redis = handleTest ./redis.nix {};
   redmine = handleTest ./redmine.nix {};
   restic = handleTest ./restic.nix {};
